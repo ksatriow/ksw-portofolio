4c2eeba7885870191fc312595ae5410cf7704e18
“Kukuh <kukuhsatriowibowo@gmail.com>
2026-03-29 11:54:38 +0700

    fix: resolve certificate update redirect bug and add project/certificate pagination

--- controller/cmsController.js
+++ controller/cmsController.js
@@ -1,94 +1,52 @@
 const { Certificate } = require("../models");
 
 module.exports = {
-  index: (req, res) => {
-    Certificate.findAll({}).then((article) => {
+  index: async (req, res) => {
+    try {
+      const article = await Certificate.findAll({});
       res.render("./cms/index", { article });
-    });
+    } catch (e) {
+      res.redirect("/");
+    }
   },
   create: (req, res) => res.render("./cms/create"),
-  doCreate: (req, res) => {
-    const { title, description } = req.body;
-
-    Certificate.create({
-      title,
-      description,
-    }).then((article) => {
-      res.render("./cms/index", { article });
-    });
-  },
-  update: (req, res) => res.render("./cms/update/", { article }),
-  doUpdate: (async (req, res) => {
-    // // cara 1
-    // const { title, description } = req.body;
-    // const userId = req.params.id;
-    // const query = {
-    //   where: { id: userId },
-    // };
-    // Certificate.update({ title, description }, query)
-    //   .then(() => {
-    //     // req.flash('success', 'Book successfully updated');
-    //     // res.redirect('./cms');
-    //     res.status(201).json("Update succesfully");
-    //   })
-    //   .catch((err) => {
-    //     res.status(422).json("Can't update book");
-    //   });
+  doCreate: async (req, res) => {
     try {
       const { title, description } = req.body;
-      const userId = req.params.id;
-      const query = {
-        where: { id: userId },
-      };
-
-      const project = await Certificate.update(
-        {
-          title: title,
-          description: description,
-        },
-        query
-      );
-      return res.redirect('/cms/update/' + userId);
-    } catch (error) {
-      console.log(error)
-      // return res.redirect('/cms/update/'+req.params.id);
+      await Certificate.create({ title, description });
+      res.redirect("/cms");
+    } catch (e) {
+      console.log(e);
+      res.redirect("/cms/create");
     }
-  }),
-  delete: (req, res) => res.render("./cms/delete"),
-  doDelete: (req, res) => {
-    const userId = req.params.id;
-
-    Certificate.destroy({
-      where: { id: userId },
-    })
-      .then((data) => {
-        res.json({
-          status: 200,
-          message: "DATA BERHASIL DIHAPUS",
-          data: userId,
-        });
-      })
-      .catch((err) => {
-        res.json({
-          status: 400,
-          message: "DATA GAGAL DIHAPUS",
-        });
-      });
   },
-  detail: (req, res) => {
-    const articleId = req.params.id;
-
-    Certificate.findOne({
-      where: { id: articleId },
-    })
-      .then((article) => {
-        res.render("./cms/update", { article });
-      })
-      .catch((err) => {
-        res.json({
-          status: 400,
-          message: "data tidak ditemukan",
-        });
-      });
+  detail: async (req, res) => {
+    try {
+      const article = await Certificate.findOne({ where: { id: req.params.id } });
+      res.render("./cms/update", { article });
+    } catch (e) {
+      res.redirect("/cms");
+    }
+  },
+  doUpdate: async (req, res) => {
+    try {
+      const { title, description } = req.body;
+      await Certificate.update(
+        { title, description },
+        { where: { id: req.params.id } }
+      );
+      res.redirect("/cms");
+    } catch (e) {
+      console.log(e);
+      res.redirect("/cms");
+    }
+  },
+  doDelete: async (req, res) => {
+    try {
+      await Certificate.destroy({ where: { id: req.params.id } });
+      res.redirect("/cms");
+    } catch (err) {
+      res.redirect("/cms");
+    }
   },
 };
--- controller/homeController.js
+++ controller/homeController.js
@@ -4,12 +4,36 @@ module.exports = {
   // index:(req,res) => res.render('./home')
   index: async (req, res) => {
     try {
-      const certificate = await Certificate.findAll({});
-      const projects = await Project.findAll({});
-      res.render("./home", { certificate, projects });
+      const pPage = parseInt(req.query.projectPage) || 1;
+      const cPage = parseInt(req.query.certPage) || 1;
+      const pLimit = 4;
+      const cLimit = 6;
+
+      const { count: countCert, rows: certificate } = await Certificate.findAndCountAll({
+        limit: cLimit,
+        offset: (cPage - 1) * cLimit,
+        order: [['id', 'DESC']]
+      });
+
+      const { count: countProj, rows: projects } = await Project.findAndCountAll({
+        limit: pLimit,
+        offset: (pPage - 1) * pLimit,
+        order: [['id', 'DESC']]
+      });
+
+      const totalProjectPages = Math.ceil(countProj / pLimit);
+      const totalCertPages = Math.ceil(countCert / cLimit);
+
+      res.render("./home", {
+        certificate, projects,
+        pPage, cPage, totalProjectPages, totalCertPages
+      });
     } catch (e) {
       console.log(e);
-      res.render("./home", { certificate: [], projects: [] });
+      res.render("./home", {
+        certificate: [], projects: [],
+        pPage: 1, cPage: 1, totalProjectPages: 0, totalCertPages: 0
+      });
     }
   },
 }
